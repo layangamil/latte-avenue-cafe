cc16395f2ba6b2070ff97229014cd263448f621b
Ayan Ibar <Ayanibar@gmail.com>
2026-01-26 10:25:37 +0100

    Add gitignore for backend local files

--- backend/server.js
+++ backend/server.js
@@ -1,61 +1,38 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 const app = express();
-
-const menu = [
-  {
-        id: 1,
-        name: "Chocolate Latte",
-        price: 35,
-        category: "drink",
-        ingredients: ["Whole milk", "Dark chocolate"]
-    },
-    {
-        id: 2,
-        name: "Brown Sugar Latte",
-        price: 30,
-        category: "drink",
-        ingredients: ["Whole milk", "Egg whites"]
-    },
-    {
-        id: 3,
-        name: "Classic Espresso",
-        price: 25,
-        category: "drink",
-        ingredients: ["Double espresso shots"]
-    },
-    {
-        id: 4,
-        name: "Lemon Curd Macaron",
-        price: 15,
-        category: "dessert",
-        ingredients: ["Flour", "Egg", "Whole milk"]
-    },
-    {
-        id: 5,
-        name: "Strawberry Short Cake",
-        price: 30,
-        category: "dessert",
-        ingredients: ["Flour", "Whole milk"]
-    },
-    {
-        id: 6,
-        name: "Berry Cheese Cake",
-        price: 25,
-        category: "dessert",
-        ingredients: ["Flour", "Egg"]
-    }
-    ];
-
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Latte Avenue backend is running');
+//MySQL connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Ibar2001Ayan',
+    database: 'latte_avenue_cafe'
 });
 
+db.connect((err) => {
+    if (err) {
+        console.error('Database connection failed:', err);
+        return;
+    }
+    console.log('Connected to MySQL database!');
+});
+
+//Test endpoint
+app.get('/', (req, res) => {
+    res.send('Latte Avenue backend is running');
+});
+
+//Get menu from database
 app.get('/api/menu', (req, res) => {
-    res.json(menu);
+    db.query('SELECT * FROM product', (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
