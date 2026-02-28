24b1e8c690dc0d00866d2eb355e42525e49b3ba8
Layan Gamil <layan.gamil@gmail.com>
2026-02-28 19:37:15 +0100

    Fixed index.html and created menu-disply.js for dynamic menu items

---
+++ frontend/js/menu-display.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function(){
+    loadPublicMenu();
+});
+
+async function loadPublicMenu(){
+    try {
+        const response = await fetch('http://localhost:5000/api/items');
+        const items = await response.json();
+        
+        const drinks = items.filter(item => item.category?.toLowerCase()==='drink');
+        const desserts = items.filter(item => item.category?.toLowerCase()==='dessert');
+
+        displayMenuItems('drinks-menu', drinks);
+        displayMenuItems('desserts-menu', desserts);
+
+    } catch(error){
+        console.log('Error:', error);
+    }
+}
+
+function displayMenuItems(containerId, items){
+    const container = document.getElementById(containerId);
+
+    if (!container) return;
+
+    if(items.length === 0){
+        container.innerHTML = '<p>No items available</p>';
+        return;
+    }
+
+    let html = '';
+    for (let i = 0; i < items.length; i++){
+        const item = items[i];
+        html += `
+            <div class="menu-item">
+                <img src="${item.image_url || 'https://placehold.co/600x400/000000/FFF'}" alt="${item.name}">
+                <div class="item-content">
+                    <h4>${item.name}</h4>
+                    <p>Includes: ${item.ingredients || 'Nothing to specify :)'}</p>
+
+                    <div class="item-footer">
+                        <span class="price">${item.price} SEK</span>
+                        <button class="btn-add" data-id="${item.product_id}" data-name="${item.name}" data-price="${item.price}">
+                            <i class="fas fa-plus"></i>+ Add
+                        </button>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+    container.innerHTML = html;
+}
+
+window.addEventListener('storage', function(e){
+    if (e.key === 'menuUpdated'){
+        loadPublicMenu();
+    }
+});
